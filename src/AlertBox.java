5439644a7da306b358119d85c66ace5cd60c2d69
juliusbacosa <juliusbacosa@gmail.com>
2016-01-17 13:00:20 +0800

    Initial commit

---
+++ src/AlertBox.java
@@ -0,0 +1,10 @@
+import javafx.stage.*;
+/**
+ * Created by Butchokoi on 1/17/2016.
+ */
+public class AlertBox {
+    public static void display(String title,String message){
+        Stage window = new Stage();
+        //
+    }
+}
