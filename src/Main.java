6683cef8eecd9d354640110e0443c9ef95042455
juliusbacosa <juliusbacosa@gmail.com>
2016-01-17 13:08:30 +0800

    - alertBox added

--- src/Main.java
+++ src/Main.java
@@ -29,9 +29,14 @@ public class Main extends Application{
             window.setScene(scene2);
         });
 
+        //button 3
+        Button button3 = new Button("This is for Alert Box");
+        button3.setOnAction(e -> AlertBox.display("Title of Window","Wow this is a cool alertBox"));
+
         //Layout 1 - children are laid out in vertical column
         VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
+        layout1.getChildren().addAll(label1, button1, button3);
+
         scene1 = new Scene(layout1,200,200);
 
 
@@ -43,6 +48,9 @@ public class Main extends Application{
         StackPane layout2 = new StackPane();
         layout2.getChildren().add(button2);
         scene2 = new Scene(layout2,600,200);
+
+
+
 
         window.setScene(scene1);
         window.setTitle("Title here");
