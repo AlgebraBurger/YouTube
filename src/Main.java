81b3d76fe297c76bc433814e739eb27033392f0c
juliusbacosa <juliusbacosa@gmail.com>
2016-01-17 13:40:04 +0800

    - added alertbox and closing program

--- src/Main.java
+++ src/Main.java
@@ -33,9 +33,27 @@ public class Main extends Application{
         Button button3 = new Button("This is for Alert Box");
         button3.setOnAction(e -> AlertBox.display("Title of Window","Wow this is a cool alertBox"));
 
+        //button 4
+        Button button4 = new Button("Confim Box");
+        button4.setOnAction(e-> {
+            boolean result = ConfirmBox.display("Confirm Box Title","Are you sure bro?");
+            System.out.println(result);
+        });
+
+        window.setOnCloseRequest(e->{
+            e.consume();
+            closeProgram();
+        });
+
+        //button 5
+        Button button5 = new Button("Close Application");
+        button5.setOnAction(e->{
+            closeProgram();
+        });
+
         //Layout 1 - children are laid out in vertical column
         VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1, button3);
+        layout1.getChildren().addAll(label1, button1, button3, button4, button5);
 
         scene1 = new Scene(layout1,200,200);
 
@@ -49,13 +67,24 @@ public class Main extends Application{
         layout2.getChildren().add(button2);
         scene2 = new Scene(layout2,600,200);
 
-
-
-
         window.setScene(scene1);
         window.setTitle("Title here");
         window.show();
 
 
+    }
+
+    private void closeProgram(){
+
+        Boolean answer = ConfirmBox.display("Title","Are you sure you want to exit?");
+
+        if(answer){
+            window.close();
+        }
+
+        /*
+        System.out.println("File is Saved");
+        window.close();
+        */
     }
 }
