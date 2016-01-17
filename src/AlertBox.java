3215724ad821ea40f3e3bc64aac0969a4e65109d
juliusbacosa <juliusbacosa@gmail.com>
2016-01-17 13:04:48 +0800

    - init commit

--- src/AlertBox.java
+++ src/AlertBox.java
@@ -1,3 +1,8 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.*;
 /**
  * Created by Butchokoi on 1/17/2016.
@@ -5,6 +10,24 @@ import javafx.stage.*;
 public class AlertBox {
     public static void display(String title,String message){
         Stage window = new Stage();
-        //
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+        Label label = new Label();
+        label.setText(message);
+
+        Button closeButton = new Button("Close the window");
+        closeButton.setOnAction(e -> window.close());
+
+        VBox layout = new VBox();
+        layout.getChildren().addAll(label, closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+
     }
 }
